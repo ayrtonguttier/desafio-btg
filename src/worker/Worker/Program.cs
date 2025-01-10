93d41c348b87ca788f623aa9af6524c4255d4e28
= <ayrtonsg@hotmail.com>
2025-01-10 17:52:13 -0300

    worker first iteration

---
+++ src/worker/Worker/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "fila", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+
+consumer.ReceivedAsync += async (ch, ea) => {
+  var body = ea.Body.ToArray();
+  var json = Encoding.UTF8.GetString(body);
+  Console.WriteLine(json);
+  await channel.BasicAckAsync(ea.DeliveryTag, false);
+};
+
+await channel.BasicConsumeAsync(queue: "fila", autoAck: false, consumer: consumer);
+
+await Task.Delay(TimeSpan.FromSeconds(30));
